fbf8bbed86e992b633b57a8feb682aa2a9ed7685
anay-joshi26 <abjoshi2612@gmail.com>
2023-04-24 16:41:56 +0800

    dahsdahwd

--- XtremeCoders/test_addition.py
+++
@@ -1,9 +0,0 @@
-def add(a, b):
-    """Add a and b"""
-    return a+b
-
-
-def test_add():
-    assert add(1, 1) == 2
-    assert add(0, 0) == 0
-    assert add(1, -1) == 0
--- XtremeCoders/test_subtraction.py
+++
@@ -1,9 +0,0 @@
-def subtract(a, b):
-    """subtract b from a"""
-    return a-b
-
-
-def test_subtract():
-    assert subtract(1, 1) == 0
-    assert subtract(0, 1) == -1
-    assert subtract(0, -1) == 1
